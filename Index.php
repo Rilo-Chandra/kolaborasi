94bb6250aa77b9e165a801ad864b6c3ac5873b55
meirizqina <215410118@students.akakom.ac.id>
2021-10-19 07:40:03 +0700

    menambah index

---
+++ Index.php
@@ -0,0 +1,6 @@
+
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h3> Isi Nama dan Nim Anda di bawah baris code ini </h3>";
+echo "<h3> Mei Rizqinaa Z.L (215410118) </h3>"
+?>
